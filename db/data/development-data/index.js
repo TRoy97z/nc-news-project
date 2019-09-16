7d1e504c691d3b1972965f460bb6aeb86b23b489
Troy Teelucksingh <troy@troys-mbp-2.home>
2019-09-16 23:47:45 +0100

    removed gitignore file and made index files

---
+++ db/data/development-data/index.js
@@ -0,0 +1,4 @@
+exports.articleData = require("../development-data/articles");
+exports.topicData = require("../development-data/topics");
+exports.userData = require("../development-data/users");
+exports.commentData = require("../development-data/comments");
