7e6b2d2e6be35dd540bca6356ff04afebcfe923a
Troy Teelucksingh <troy@troys-mbp-2.home>
2019-09-18 00:00:20 +0100

    added migrate make script, and migration files

---
+++ db/migrations/20190917222110_articles_table.js
@@ -0,0 +1,21 @@
+exports.up = knex => {
+  return knex.schema.createTable("articles", articlesTable => {
+    articlesTable.increments("article_id").primary();
+    articlesTable.string("title").notNullable();
+    articlesTable.text("body").notNullable();
+    articlesTable.integer("votes").defaultTo(0);
+    articlesTable
+      .string("topic")
+      .notNullable()
+      .references("topics.slug");
+    articlesTable
+      .string("author")
+      .notNullable()
+      .references("users.username");
+    articlesTable.timestamp("created_at");
+  });
+};
+
+exports.down = knex => {
+  return knex.schema.dropTable("articles");
+};
