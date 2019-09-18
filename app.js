7fbb0ea8358b23652826b98883c4f9469eb6e793
Troy Teelucksingh <troy@Troys-MacBook-Pro-2.local>
2019-09-18 16:15:15 +0100

    added files: app, app-spec, connection and api router

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const apiRouter = require("./routers/api");
+
+app.use(express.json());
+app.use("/api", apiRouter);
+
+module.exports = app;
