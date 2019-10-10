ac080bf1bbf322b9aa4745ba2c89813e5893156d
Troy Teelucksingh <troy@Troys-MacBook-Pro-2.local>
2019-10-10 11:35:21 +0100

    removed created_at

--- models/commentsM.js
+++ models/commentsM.js
@@ -1,9 +1,9 @@
 const connection = require("../db/connection");
 
 exports.insertComment = (article_id, newComment) => {
-  const { username, body, created_at } = newComment;
+  const { username, body } = newComment;
   return connection
-    .insert({ author: username, body, article_id, created_at })
+    .insert({ author: username, body, article_id })
     .into("comments")
     .returning("*");
 };
